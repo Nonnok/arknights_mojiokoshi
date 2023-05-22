2e3b59270183a68838b5d1589df6df88b5862d67
Nonnok <donntenn0917@icloud.com>
2023-05-22 22:43:22 +0900

    New chapters and extra elements.

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ btn.onclick = () => {
 mask.onclick = () => {
   nav.classList.toggle("open");
 }
+
+{
+  const talk = document.getElementById('operater');
+  const voice = document.getElementById('voice');
+  const conversation = [
+    "ロドスは全艦通常航行モードです。ドクター、航行スケジュールをチェックしませんか？　▼",
+    "ケルシー先生が言ってました、仕事をする時は集中しなければいけない、と……。うん、集中しなきゃ。　▼",
+    "ドクター、またあなたと一緒に戦えて、本当に嬉しいです！　▼",
+    "みなさんの期待に応えて見せます！　▼",
+    "何か飲みませんか、ドクター？　▼",
+    "えへへ……　▼",
+    "ドクター、お仕事お疲れ様です。　▼",
+    "なるほど……本当に役に立つ知識ばかりです。ドクター、ご指導いただきありがとうございます！　▼",
+    "ドクター、私たちの歩む道は、終わりなき旅路なのかもしれません……でも、それをドクターと共に歩むことができて、私はとても幸せです。　▼"
+  ];
+
+  talk.addEventListener('click', () => {
+    const randomText = Math.floor(Math.random() * 9);
+    voice.textContent = conversation[randomText];
+  });
+}
